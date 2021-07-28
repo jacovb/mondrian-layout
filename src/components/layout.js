0bf5a048cb4d1aed4cb58062e1e90daa62ba790e
jacovb <vbiljon@gmail.com>
2021-07-28 18:02:09 +0100

    started mondrian grid

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,31 +3,36 @@ import { Link } from "gatsby"
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <main className="mx-auto bg-gray-300 h-screen w-screen">
+    <main className="mx-auto bg-gray-900 h-screen w-screen">
       <title>{pageTitle}</title>
-      <nav className="flex">
-        <Link
-          to="/"
-          className="flex-auto bg-red-400 text-black text-center mt-16 py-1 mx-8 rounded-lg hover:bg-red-700 hover:text-white"
-        >
-          Home
-        </Link>
+      <div id="grid" className="grid grid-cols-12 grid-rows-6 w-full h-full">
+        <h1 className="text-white">Hello</h1>
+        <div className="row-start-2 row-end-3 col-start-3 col-end-9 bg-white ">
+          <nav className="flex space-x-1.5 mx-4 my-4 items-center">
+            <Link
+              to="/"
+              className="flex-auto bg-red-400 text-white text-center py-1 hover:bg-red-700 hover:text-white"
+            >
+              Home
+            </Link>
 
-        <Link
-          to="/about"
-          className=" flex-auto bg-red-400 text-black text-center mt-16 py-1 mx-8 rounded-lg hover:bg-red-700 hover:text-white"
-        >
-          About
-        </Link>
+            <Link
+              to="/about"
+              className="flex-auto bg-red-400 text-white text-center py-1 hover:bg-red-700 hover:text-white"
+            >
+              About
+            </Link>
 
-        <Link
-          to="/page-2"
-          className=" flex-auto bg-red-400 text-black text-center mt-16 py-1 mx-8 rounded-lg hover:bg-red-700 hover:text-white"
-        >
-          Page-2
-        </Link>
-      </nav>
-      <h1>{pageTitle}</h1>
+            <Link
+              to="/page-2"
+              className="flex-auto bg-red-400 text-white text-center py-1 hover:bg-red-700 hover:text-white"
+            >
+              Page-2
+            </Link>
+          </nav>
+        </div>
+      </div>
+      <h1 className="text-white">{pageTitle}</h1>
       {children}
     </main>
   )
